1e5ce1f57afc2afc85d83e954b494b1b5270eace
OlyaKyrylyuk <olya_kyrylyuk15@ukr.net>
2021-03-16 23:41:37 +0200

    added LR2 - first task

---
+++ app/src/main/java/com/example/myapplication/AboutProgram_Activity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutProgram_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_program_);
+    }
+
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.myapplication;
-
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,14 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView surname_name = findViewById(R.id.surname_name);
-        final TextView group = findViewById(R.id.group);
         final ImageButton ok_button = findViewById(R.id.ok_button);
 
         ok_button.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
-                surname_name.setText("Кирилюк Ольга Олександрівна");
-                group.setText("Група - ІПЗс-19-1");
+                Intent intent = new Intent(MainActivity.this, AboutProgram_Activity.class);
+                startActivity(intent);
             }
         });
     }
